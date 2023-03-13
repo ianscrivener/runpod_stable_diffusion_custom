15e156a937d970af75c4a9a2b9ece66c70043fb1
Ian Scrivener <git@cleverheart.io>
2023-03-13 12:41:41 +1100

    tweak paths

--- _DevOps_scripts/pull_build_push.sh
+++ _DevOps_scripts/pull_build_push.sh
@@ -3,6 +3,7 @@
 cd ..
 git restore .
 git pull origin main
+docker image rm *
 docker build -t ianscrivener/runpod_stable_diffusion_custom .
 docker image ls
 docker push ianscrivener/runpod_stable_diffusion_custom
